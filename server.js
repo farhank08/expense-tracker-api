68a0e3e527e5699c1fe8a0b8e9662de54e8d0fb2
Farhan Khorsheed <ratul097@gmail.com>
2025-12-11 00:28:07 +1100

    Add database client initialization and shutdown, view router and api routers

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+import { initDb } from './src/databases/dbClient.js';
+import UserRouter from './src/routers/apiRouters/userRouter.js';
+import ExpenseRouter from './src/routers/apiRouters/expenseRouter.js';
+import ViewRouter from './src/routers/viewRouter.js';
+
 // Load environment variables
 dotenv.config();
+
+try {
+	// Connect to database
+	await initDb();
+} catch (error) {
+	// Handle database connection error
+	console.error(error.message);
+
+	// Exit with error
+	process.exit(1);
+}
 
 // Resolve file path in ES Module
 const __filename = fileURLToPath(import.meta.url);
@@ -30,6 +47,24 @@ app.get('/favicon.ico', (req, res) => res.sendStatus(204));
 // Serve static files
 app.use(express.static(publicDir));
 
+// Serve User API routes
+app.use('/api', UserRouter);
+
+// Serve Expense API routes
+app.use('/api', ExpenseRouter);
+
+// Serve view routes
+app.use('/', ViewRouter);
+
+// Handle unhandled routes
+app.use((req, res) => {
+	console.error(`Unhandled route ${req.path}`);
+	return res.status(404).json({
+		success: false,
+		message: 'Route not found',
+	});
+});
+
 // Listen on server port
 const server = app.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
@@ -38,9 +73,22 @@ const server = app.listen(port, () => {
 // Handle server shutdown
 process.on('SIGINT', async () => {
 	try {
+		// Close express server application
 		await new Promise((resolve) => server.close(resolve));
+		console.log('Server shutdown successful');
 	} catch (error) {
+		// Handle server shutdown error
 		console.error(`Shutdown error: ${error.message}`);
+	}
+
+	try {
+		// Close mongoose client
+		await mongoose.disconnect();
+		console.log('Database shutdown successful');
+	} catch (error) {
+		// Exit with error
+		console.error(`Database shutdown failed: ${error.message}`);
+		process.exit(1);
 	} finally {
 		process.exit(0);
 	}
